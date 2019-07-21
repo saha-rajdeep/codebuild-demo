89413c8cb6ec59be622e3b289d5bbf1de7657068
EC2 Default User <ec2-user@ip-172-31-59-14.ec2.internal>
2019-07-21 01:16:00 +0000

    added code and buildspec

---
+++ lambda_function.py
@@ -0,0 +1,33 @@
+import json
+import boto3
+import requests
+
+
+def lambda_handler(event, context):
+    # TODO implement
+    # api-endpoint
+    print(event)
+    URL = "http://maps.googleapis.com/maps/api/geocode/json"
+    location = event['landmark']
+    # defining a params dict for the parameters to be sent to the API
+    PARAMS = {'address':location}
+ 
+    # sending get request and saving the response as response object
+    r = requests.get(url = URL, params = PARAMS)
+ 
+    # extracting data in json format
+    data = r.json()
+ 
+ 
+    # extracting latitude, longitude and formatted address 
+    # of the first matching location
+    latitude = data['results'][0]['geometry']['location']['lat']
+    longitude = data['results'][0]['geometry']['location']['lng']
+    formatted_address = data['results'][0]['formatted_address']
+ 
+    # printing the output
+    print("Latitude:%s\nLongitude:%s\nFormatted Address:%s"
+      %(latitude, longitude,formatted_address))
+    output={"address":formatted_address} 
+    return output
+    #return('Hello from %s'%formatted_address)
